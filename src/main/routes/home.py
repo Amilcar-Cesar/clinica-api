baf093ee6ffafb7c4418cca191cfdde6d690acea
Amilcar-Cesar <amilcarcesar0@yahoo.com.br>
2025-10-13 15:22:07 -0300

    alteração na dinamica de tabelas na pagina home

--- src/main/routes/home.py
+++ src/main/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required
 from src.main.models.atendimentos_model import Atendimentos
 from src.main.models.pacientes_model import Pacientes
@@ -11,7 +11,15 @@ home_route_bp = Blueprint("home_route", __name__)
 @home_route_bp.route('/', methods=['GET'])
 @login_required
 def home():
+    
+    view = request.args.get('view', 'atendimentos', type=str)
+
     atendimentos = Atendimentos.query.order_by(Atendimentos.data_hora.desc()).all()
     especialidades = Especialidades.query.order_by(Especialidades.nome_especialidade).all()
-    
-    return render_template('home.html',atendimentos=atendimentos, especialidades=especialidades)
+    pacientes = Pacientes.query.order_by(Pacientes.nome).all()
+
+    return render_template('home.html',
+                            atendimentos=atendimentos,
+                            especialidades=especialidades,
+                            pacientes=pacientes,
+                            view=view)
